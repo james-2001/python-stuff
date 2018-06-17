d0286d424ef1e3421c1b41692025c153168af587
James May <james.a.may.2001@gmail.com>
2018-06-17 21:54:53 +0100

    added increasing size of doors

--- montyhallmontecarlo.py
+++ montyhallmontecarlo.py
@@ -2,23 +2,30 @@ import random
 counter = 1
 stickcounter = 0
 switchcounter = 0
+doornumber = 3
+switchwon= 0
+doorcount =0
+while doorcount < 100000000:
+    # loop lots of times...
+    while counter < 1000:
+        contdoor = random.randint(1, doornumber)
+        prizedoor = random.randint(1, doornumber)
+        opendoor = 1
 
-# loop lots of times...
-while counter < 1000:
-    contdoor = random.randint(1, 3)
-    prizedoor = random.randint(1, 3)
-    opendoor = 1
-
-    # find the first door which doesn't have the prize and isn't the one the contestant chose
-    while opendoor == prizedoor or opendoor == contdoor:
-        opendoor += 1
+        # find the first door which doesn't have the prize and isn't the one the contestant chose
+        while opendoor == prizedoor or opendoor == contdoor:
+            opendoor += 1
     
-    # find out whether the contestant would be better off switching or sticking
-    if contdoor == prizedoor:
-        stickcounter += 1
-    else:
-        switchcounter += 1
-    counter += 1
+        # find out whether the contestant would be better off switching or sticking
+        if contdoor == prizedoor:
+            stickcounter += 1
+        else:
+            switchcounter += 1
+        counter += 1
+    if switchcounter > stickcounter:
+        switchwon += 1
+    doorcount +=1
 
-print("switch ", switchcounter, " times")
-print("stick ", stickcounter, " times")
+
+print ("switch won ", switchwon, " times" )
+
