edcf370762a342dfe79d4893385759c03a9e44ec
James May <james.a.may.2001@gmail.com>
2018-06-20 11:21:29 +0100

    reset counter to 0 after after each door number sim, added counters, included potential to switch and still lose

--- hello.py
+++ hello.py
@@ -1,22 +1,8 @@
-import plotly.plotly as py
-import plotly.graph_objs as go
-
-# Create random data with numpy
-import random
-
-random_x = random.randint(1,10)
-random_y = random.randint(1,10)
-
-# Create a trace
-trace = go.Scatter(
-    x = random_x,
-    y = random_y,
-    mode = 'markers'
-)
-
-data = [trace]
-
-# Plot and embed in ipython notebook!
-py.iplot(data, filename='basic-scatter')
-
-# or plot with: plot_url = py.plot(data, filename='basic-line')
+var = 1
+vart = 2
+while var < 10:
+    while vart <10:
+        print ("vart" , vart)
+        vart +=1
+    var += 1
+    print ("var", var)
--- montyhallmontecarlo.py
+++ montyhallmontecarlo.py
@@ -5,12 +5,11 @@ counter = 1
 stickcounter = 0
 switchcounter = 0
 doornumber = 3
-switchwon= 0
-doorcount =0
-n= 1000001 #n number of doors you want to try up to
-while doorcount < n:
+switchwon = 0
+n = 10000  # n number of doors you want to try up to
+while doornumber < n:
     # loop lots of times...
-    while counter < 100000:
+    while counter < 10000:
         contdoor = random.randint(1, doornumber)
         prizedoor = random.randint(1, doornumber)
         opendoor = 1
@@ -18,20 +17,15 @@ while doorcount < n:
         # find the first door which doesn't have the prize and isn't the one the contestant chose
         while opendoor == prizedoor or opendoor == contdoor:
             opendoor += 1
-    
+
         # find out whether the contestant would be better off switching or sticking
         if contdoor == prizedoor:
             stickcounter += 1
-        else:
-            switchcounter += 1
+        else: 
+            if  random.randint(1, doornumber-2) == 1:
+                switchcounter += 1
         counter += 1
-    percentwon = (switchcounter/100000) 
-    
-    doorcount +=10000
-trace = go.Scatter(
-        x = doornumber,
-        y = percentwon,
-        mode = "markers"
-    )
-data = [trace]
-py.iplot(data, filename= 'monty hall')
+    percentwon = (switchcounter/stickcounter)
+    print (doornumber, stickcounter, switchcounter, percentwon)
+    doornumber += 1
+    counter =1
